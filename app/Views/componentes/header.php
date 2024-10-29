003f87f2c22b64beb0ab1b403e188885982938de
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-10-28 23:00:55 -0300

    bootstrap icons imported

--- app/Views/componentes/header.php
+++ app/Views/componentes/header.php
@@ -11,10 +11,12 @@
     <script defer src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     
     <!-- Bootstrap 5.3 -->
-    <!-- Css do Bootstrap padrão - Não utilizando em favor do bootswatch que adiciona vários estilos diferentes
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    <!-- Css do Bootstrap padrão - Não utilizando em favor do bootswatch que adiciona vários estilos diferentes -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    
     <!-- Css do bootswatch. Apenas adiciona estilos diferentes ao Bootstrap (usando flatly - use o que preferir :) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/flatly/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Scripts do Bootstrap -->
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- importando o nosso arquivo de funções auxiliares -->
